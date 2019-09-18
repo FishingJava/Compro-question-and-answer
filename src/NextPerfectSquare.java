37e5b36d9b327782be09c53d11f9355cbe0612a2
Fish <n0284422@VDDK35X-3VRIHX1.lm.lmig.com>
2019-09-18 08:34:00 -0700

    project created

---
+++ src/NextPerfectSquare.java
@@ -0,0 +1,13 @@
+
+public class NextPerfectSquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println(isPerfectSquare(10));
+	}
+	
+	private static int isPerfectSquare(int n) {
+		return 0;
+	}
+
+}
