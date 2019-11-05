e9677ddbce77dbeb03ee5fca56ab935957c3d781
FishingJava <52393106+FishingJava@users.noreply.github.com>
2019-11-05 08:39:36 -0800

    Update NextPerfectSquare.java

--- src/NextPerfectSquare.java
+++ src/NextPerfectSquare.java
@@ -7,7 +7,18 @@ public class NextPerfectSquare {
 	}
 	
 	private static int isPerfectSquare(int n) {
-		return 0;
-	}
-
-}
+	 int half = n/2;
+	 int ret = 0;
+	   //special condition or exception
+            if(n == 1) {
+		return 1;	
+		}
+	   for (int i= 0; i< half; i++) {
+		if(i*i >= n) {
+		  ret = i*i;
+		     break;
+		  }
+	   }
+	   return ret;
+       }
+  }
